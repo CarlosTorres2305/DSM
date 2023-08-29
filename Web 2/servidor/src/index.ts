72d297c312d07232950b1f2005fba8f9453fb784
CarlosTorres2305 <carlosadm15@hotmail.com>
2023-08-29 07:31:03 -0300

    atualizações 29-08

--- Web 2/servidor/src/index.ts
+++ Web 2/servidor/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import {sum,dif} from "./operacao"
-import { dayOfMonth, month, year } from "./date";
+import {sum,dif} from "./controllers/operacao"
+import { dayOfMonth, month, year } from "./controllers/date";
+import routes from "./routes";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -10,10 +11,11 @@ const app = express();
 app.use(express.json());
 app.listen(PORT, function(){ console.log(`Rodando ${PORT}`)});
 
-app.get("/somar/:x/:y", sum);
 
-app.post("/subtrair", dif);
 
-app.get("/dia", dayOfMonth);
-app.get("/mes", month);
-app.get("/ano", year);
+
+app.use(routes);
+
+app.use(function(req,res){
+    res.send("Rota Desconhecida")
+});
